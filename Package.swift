a9f36a206943bef2056fa1c833a63fde8fe5aa58
Max Toskhoparan <max@enovlab.com>
2025-04-08 15:36:18 -0400

    init

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "YoopSdk",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "YoopSdk", targets: ["YoopSdk"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "YoopSdk",
+         url: "<link to the uploaded XCFramework ZIP file>",
+         checksum:"5951f519d9c42f7b99d1b09a3e0871b23659ccab3fe12c97e5666876e3981b64")
+   ]
+)
